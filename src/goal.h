91316377c62829f44a04d1beb8c762405de07c03
Noah Hradek <noahh717@gmail.com>
2015-04-05 02:58:21 -0400

    files

---
+++ src/goal.h
@@ -0,0 +1,13 @@
+#pragma once
+		
+typedef struct Rect_S{
+		Vector2 pos;
+		Vector2 size;
+} Rect;
+ 
+typedef struct Goal_S {
+    Rect rect;
+		bool active;
+		bool has_next;
+		Goal next;
+} Goal;
